5a10bcfc7e52edc04a9fd6f0c02d44a6799a3d7f
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-15 11:39:56 +0200

    Added functionality to edit blogpost

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,9 +4,22 @@ import React from "react";
 const blogReducer = (state, action) => {
     switch(action.type) {
         case 'add_blogpost':
-            return [...state, {id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content}]
+            return [
+                ...state, 
+                {
+                    id: Math.floor(Math.random() * 99999), 
+                    title: action.payload.title, 
+                    content: action.payload.content
+                }
+            ]
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload)
+        case 'edit_blogpost':
+            return state.map(blogPost => {
+                return blogPost.id === action.payload.id 
+                ? action.payload 
+                : blogPost
+            });
         default: 
             return state;
     }
@@ -15,7 +28,7 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload:{title, content}})
-        callback()
+        if(callback) callback()
     }
 }
 
@@ -25,8 +38,18 @@ const deleteBlogPost = (dispatch) => {
     }
 }
 
+const editBlogPost = (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({ 
+            type: 'edit_blogpost', 
+            payload: {id, title, content}
+        })
+        if(callback) callback()
+    }
+}
+
 export const {Context, Provider } = createDataContext(
     blogReducer,
-    {addBlogPost, deleteBlogPost}, 
+    {addBlogPost, deleteBlogPost, editBlogPost}, 
     [{title: "TEST POST", content: "TEST CONTENT", id: -1}]
 )
