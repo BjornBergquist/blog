f9ed2896ac9d96af6ad1358a04c78f48f15dc807
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-15 10:18:46 +0200

    Button for creating blog post now creates a new blog post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,16 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState, useContext} from 'react'
 import { StyleSheet, Text, TextInput, View, Button} from 'react-native'
+import { Context } from '../context/BlogContext'
 
 const CreateScreen = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const {addBlogPost} = useContext(Context)
     return (
         <View>
             <Text style={styles.label}>Enter Title: </Text>
             <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
             <Text style={styles.label}>Enter Content: </Text>
             <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-            <Button title="Add Blog Post"/>
+            <Button
+                onPress={() => addBlogPost(title, content)} 
+                title="Add Blog Post"
+            />
         </View>
     )
 }
